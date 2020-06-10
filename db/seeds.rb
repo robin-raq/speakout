d30a3593539fafbfc359917e596909720923d87c
robin-raq <robin.raq@gmail.com>
2020-06-09 22:23:02 -0400

    installed and setup devise and simple_form gems, migrated and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+### Example of creating 100 test posts
+# 100.times do
+# 	Post.create(title:'text', body: 'more works')
+# end
+
+User.create!(email: 'raq@example.com', password: 'pass123', password_confirmation: 'pass123', admin: true)
